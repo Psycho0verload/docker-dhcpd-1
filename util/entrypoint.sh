0f4079c7abe4456402f8b85207fb1be6a9853963
alinmear <alinmear@users.noreply.github.com>
2017-06-28 09:43:27 +0200

    Add several checks to entrypoint.sh, move leases file to PROJECT_HOME

--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -12,5 +12,13 @@ local _dhcpd_conf="$PROJECT_HOME/dhcpd.conf"
 [ ! -f $_dhcpd_conf ] && echo "$_dhcpd_conf not found. Exiting ..." && \
 exit 1
 
+[ ! -f $PROJECT_HOME/dhcpd.leases ] && touch $PROJECT_HOME/dhcpd.leases
+
+# borrowed from https://github.com/networkboot/docker-dhcpd/blob/master/util/entrypoint.sh
+container_id=$(grep docker /proc/self/cgroup | sort -n | head -n 1 | cut -d: -f3 | cut -d/ -f3)
+if perl -e '($id,$name)=@ARGV;$short=substr $id,0,length $name;exit 1 if $name ne $short;exit 0' $container_id $HOSTNAME; then
+    echo "You must add the 'docker run' option '--net=host' if you want to provide DHCP service to the host network."
+fi
+
 # start supervisord
 "$@"
